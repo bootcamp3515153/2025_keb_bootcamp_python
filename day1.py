dbaebcee0a49ae376f953ba0f18851880e881af9
JEONGUK <jung010209@inha.edu>
2025-02-11 10:48:09 +0900

    v0.5 day02 primeNum

--- day1.py
+++ day1.py
@@ -1,7 +1,12 @@
-# for dan in range(2,10,1):
-#     for i in range(1,10,1):
-#         print(f"{dan} * {i} = {dan*i}")
+n = int(input("Input number: "))
 
-dan = int(input("Input dan : "))
-for i in range(2,10,1):
-    print(f"{dan} * {i} = {dan * i}")
+count = 0
+
+for i in range(1, n+1):
+    if n % i == 0:
+        count = count + 1
+
+if count == 2 :
+    print(f"{n} is prime number")
+else:
+    print(f"{n} is NOT prime number")
