4419b38f42af1bd8bc3096a7f263108db33080ba
JEONGUK <jung010209@inha.edu>
2025-02-11 13:41:37 +0900

    v09 day02 primeNum use define

--- day1.py
+++ day1.py
@@ -1,18 +1,29 @@
+
+def is_prime(num) -> bool:
+    """
+    A function that returns True if it is a prime number and False if it is not a prime number
+    :param num: integer number
+    :return: boolean type
+    """
+    if num >= 2:
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                return False
+                #is_prime = False
+                #break
+            #print(i, end=' ')
+    else:
+        return False
+    return True
+# main
+#help(abs)
+help(is_prime)
+
 n = int(input("Input number: "))
 
-is_prime = True
-num = int(n**0.5)
 
-
-if n>= 2:
-    for i in range(2, num+1):
-        if n % i == 0:
-            is_prime = False
-            break
-else :
-    is_prime = False
-
-if is_prime == True :
+if is_prime(n) :
     print(f"{n} is prime number")
 else:
     print(f"{n} is NOT prime number")
+
