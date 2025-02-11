07cdc93ea757efd45d81dfa3255e25cbcea22513
JEONGUK <jung010209@inha.edu>
2025-02-11 11:35:34 +0900

    v0.7 day02 primeNum

--- day1.py
+++ day1.py
@@ -1,9 +1,11 @@
 n = int(input("Input number: "))
 
 is_prime = True
+num = int(n**0.5)
+
 
 if n>= 2:
-    for i in range(2, n):
+    for i in range(2, num):
         if n % i == 0:
             is_prime = False
             break
