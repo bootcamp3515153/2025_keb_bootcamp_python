6f34c074549ca1af944daa9d904cc8e3cd4ea9d5
JEONGUK <jung010209@inha.edu>
2025-02-13 16:19:11 +0900

    v25 day04 First Assignment

--- day04.py
+++ day04.py
@@ -1,6 +1,15 @@
-# SOLID
-# Open Closed Principle : 개방 폐쇄 원칙 (확장에는 열려 있고 수정에는 닫혀있는 원칙)
+#First Assignment
+# 2중 데코레이터 적용, 성능측정(시간측정) 데코레이터, 디스크림션 데코레이터를 팩토리얼 함수에 적용하시오.
+
 import time
+
+def discri_decorator(func):
+    def wrapper(*arg):
+        print(f'함수 이름 : {func.__name__}')
+        print(f'함수 설명 : {func.__doc__}')
+        r = func(*arg)
+        return r
+    return wrapper
 
 def time_decorator(func):
     def wrapper(*arg):
@@ -12,11 +21,17 @@ def time_decorator(func):
     return wrapper
 
 @time_decorator
+@discri_decorator
 def factorial_repetition(n) -> int:
+    """
+    Function to calculate factorial using a loop
+    :param n: Number to calculate factorial
+    :return: result
+    """
     result = 1
     for i in range(2, n+1):
         result = result * i
     return result
 
-number = int(input())
-print(f"{number}! = {factorial_repetition(number)}")
+number = int(input('Please enter a number to calculate its factorial.\n>>'))
+print(f"결과 : {number}! = {factorial_repetition(number)}")
