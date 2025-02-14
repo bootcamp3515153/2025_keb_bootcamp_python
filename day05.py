719226ae6b2ad79d7a7dc854ae04d3b70a7970f9
JEONGUK <jung010209@inha.edu>
2025-02-14 17:19:59 +0900

    v28 day05

--- day05.py
+++ day05.py
@@ -1,2 +1,5 @@
-import tgmath
 
+import tgmath
+if __name__ == "__main__" :
+    print(tgmath.tg_abs(-99))
+    print(tgmath.tg_fibonacci_loop(6))
--- tgmath.py
+++ tgmath.py
@@ -1,11 +1,39 @@
-def my_abs(n) -> int:
+def tg_abs(n) -> int:
     """
-    Return Absolute value of n
+    Return absolute value of parameter n
     :param n:
-    :return: absolute number
+    :return: absolute value
     """
-    if n <0:
+    if n < 0:
         return -n
-    else:
-        return n
+    return n
 
+
+def tg_fibonacci_recursion(n) -> int:
+    """
+    Calculate fibonacci number (ver. recursive)
+    :param n:
+    :return: result value
+    """
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return tg_fibonacci_recursion(n-2) + tg_fibonacci_recursion(n-1)
+
+
+def tg_fibonacci_loop(n) -> int:
+    """
+    Calculate fibonacci number (ver. iterative)
+    :param n:
+    :return: result value
+    """
+    n_list=[0 ,1]
+    for i in range(n+1):
+        n_list.append(n_list[i] + n_list[i + 1])
+
+    return n_list[n]
+
+if __name__ != "__main__" :
+    print("tgmath.py가 실행되었습니다.")
